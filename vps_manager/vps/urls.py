3558336f7ba75eb290924e64c963b7e5159a9710
tonboek <rushan2005@inbox.ru>
2025-02-13 23:39:12 +0300

    upd .gitignore, +requirements.txt, upd urls

--- vps_manager/vps/urls.py
+++ vps_manager/vps/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from vps.views import VPSListCreateView, VPSDetailView, index_page
 
 urlpatterns = [
-    path('', index_page),
+    # path('', index_page),
 	path('vps/', VPSListCreateView.as_view(), name='vps-list-create'),
 	path('vps/<uuid:uid>/', VPSDetailView.as_view(), name='vps-detail'),
 ]
